8c178b6212c330290e2a248b218ab556a26be1fd
JubalThang <jubalthang@gmail.com>
2022-02-01 13:04:16 -0600

    commit tree 728d955e94773a67e2421c75676cd5eca9f19271

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :no_user_found
   def show
     user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    render json: user, include: :items, status: :ok
+  end
+
+  def no_user_found 
+    render json: {error: "No user found"}, status: :not_found
   end
 
 end
